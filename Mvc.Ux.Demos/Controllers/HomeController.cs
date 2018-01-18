414d1b9fda8b405463d3244a064aaeb68828d383
Dino Esposito <dinoesp@hotmail.com>
2018-01-18 21:42:13 +0100

    1.0

--- Mvc.Ux.Demos/Controllers/HomeController.cs
+++ Mvc.Ux.Demos/Controllers/HomeController.cs
@@ -10,11 +10,16 @@
 using System.Web.Mvc;
 using Mvc.Ux.Picking.Models;
 
-namespace Mvc.Ux.Picking.Controllers
+namespace Mvc.Ux.Demos.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            var model = new ViewModelBase();
+            return View(model);
+        }
+        public ActionResult Tech()
         {
             var model = new ViewModelBase();
             return View(model);
